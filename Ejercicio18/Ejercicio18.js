5e4c2ada3fc5c474782254bb51c89086b9b17fdd
Sebastián Alej <60635537+Subkei@users.noreply.github.com>
2020-07-05 18:14:53 -0400

    Ensayo conversor de divisas
    Conversor de divisas, solo de pesos a dolares, no está totalmente implementado

---
+++ Ejercicio18/Ejercicio18.js
@@ -0,0 +1,9 @@
+function pesosDolares(valNum) {
+    document.getElementById("inputDolares").value=valNum*0.0013;
+  }
+  function dolaresPesos(valNum) {
+    document.getElementById("inputPesos").value=valNum*0.76680;
+  }
+  function pesosEuros(valNum) {
+    document.getElementById("inputEuros").value=valNum*0.86688;
+  }
