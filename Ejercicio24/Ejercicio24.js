9b13362b451d8bb7ce10b45e35eb63a3f26f9549
Sebastián Alej <60635537+Subkei@users.noreply.github.com>
2020-07-05 18:27:59 -0400

    Ensayo con librería Fancybox
    Una imagen que se maximiza, un botón para mostrar un video desde youtube y un botón para mostrar un mapa a google.

---
+++ Ejercicio24/Ejercicio24.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $("#btn-video").click(function () {
+        $("#enlace").trigger("click");
+    });
+
+    $("#btn-mapa").click(function () {
+        $("#mapa-enlace").trigger("click")
+    });
+});
